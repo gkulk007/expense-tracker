0e8fdfce21792b49ad2a682f6ffe3a015c814058
gkulk007 <gkulk007@gmail.com>
2021-09-06 18:07:05 +0530

    username validation

--- exptracker/authentication/views.py
+++ exptracker/authentication/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
 from django.views import View
+import json
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 # Create your views here.
+
+
+class UsernameValidationView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'username should only contain alpha numeric characters'})
+
+        if User.objects.filter(username=username).exists():
+            return JsonResponse({'username_error': 'Username already exists, please select a new username'})
+
+        return JsonResponse({"username_valid": True})
 
 
 class RegisterationView(View):
---
+++ exptracker/static/js/register.js
@@ -0,0 +1,29 @@
+const usernameField = document.querySelector('#usernameField');
+const feedbackArea = document.querySelector('.invalid-feedback')
+
+console.log("Connected");
+usernameField.addEventListener("keyup", (e) => {
+  // console.log('Typing...');
+
+  const usernameVal = e.target.value;
+  usernameField.classList.remove('is-invalid');
+  feedbackArea.style.display = 'none';
+
+  if (usernameVal.length > 0) {
+    fetch('/authentication/validate-username', {
+      body: JSON.stringify({
+        username: usernameVal
+      }),
+      method: 'POST',
+    }).then(res => res.json()).then(data => {
+      console.log("data", data);
+      if (data.username_error) {
+        usernameField.classList.add('is-invalid');
+        feedbackArea.style.display = 'block';
+        feedbackArea.innerHTML = `<p>${data.username_error}</p>`;
+      }
+    });
+  }
+
+
+});
