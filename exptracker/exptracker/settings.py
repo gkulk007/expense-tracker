b38d5a8efc77ad218e2d64b0b991c9265d5953cb
gkulk007 <gkulk007@gmail.com>
2021-09-09 19:03:04 +0530

    Currency Settings Done

--- exptracker/exptracker/settings.py
+++ exptracker/exptracker/settings.py
@@ -23,6 +23,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'authentication',
     'expenses',
+    'userpreferences',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
